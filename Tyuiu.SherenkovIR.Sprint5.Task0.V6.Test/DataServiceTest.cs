cb665cace4819461228ba51d3cca9f5df6cdcaf0
Sherenkov Ivan <sherenkov12@gmail.com>
2025-11-15 14:08:39 +0500

    Add project files.

---
+++ Tyuiu.SherenkovIR.Sprint5.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿
+using Tyuiu.SherenkovIR.Sprint5.Task0.V6.Lib;
+namespace Tyuiu.SherenkovIR.Sprint5.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+
+            int x = 3;
+
+            string path = @"C:\Users\user\source\repos\Tyuiu.SherenkovIR.Sprint5\Tyuiu.SherenkovIR.Sprint5.Task0.V6\bin\Debug\net8.0\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
