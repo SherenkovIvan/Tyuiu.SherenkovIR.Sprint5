e7061a8ad24456b4f496ad9a1c0bfdec1e6201ad
Sherenkov Ivan <sherenkov12@gmail.com>
2025-11-15 14:48:57 +0500

    Task1

---
+++ Tyuiu.SherenkovIR.Sprint5.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.SherenkovIR.Sprint5.Task1.V17.Lib;
+namespace Tyuiu.SherenkovIR.Sprint5.Task1.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistFile()
+        {
+            string path = @"C:\Users\user\AppData\Local\Temp\OutPutFileTask1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+
+        }
+    }
+}
