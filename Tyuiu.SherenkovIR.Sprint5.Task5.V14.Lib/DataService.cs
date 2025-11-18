6a9e6ac314fd605b7190803b9a53529b6ed0e8c6
Sherenkov Ivan <sherenkov12@gmail.com>
2025-11-18 18:54:40 +0500

    Task5

--- Tyuiu.SherenkovIR.Sprint5.Task5.V14.Lib/DataService.cs
+++ Tyuiu.SherenkovIR.Sprint5.Task5.V14.Lib/DataService.cs
@@ -1,5 +1,8 @@
-﻿using System.Globalization;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Globalization;
 using System.IO;
+using System.Runtime.Serialization;
 using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.SherenkovIR.Sprint5.Task5.V14.Lib
 {
@@ -7,33 +10,37 @@ namespace Tyuiu.SherenkovIR.Sprint5.Task5.V14.Lib
     {
         public double LoadFromDataFile(string path)
         {
-            string fileContent = File.ReadAllText(path).Trim();
-            string[] strings = fileContent.Split(' ');
+            string text = File.ReadAllText(path);
+            string[] strings = text.Split(' ');
             double[] doubles = new double[20];
             IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            for (int i = 0; i < strings.Length && i < doubles.Length; i++)
             {
-                int maxNumber = 0;
-                foreach (double num in doubles)
+                doubles[i] = double.Parse(strings[i], formatter);
+            }
+
+            int maxNumber = 0;
+            foreach (double num in doubles)
+            {
+                if (num == Math.Floor(num))
                 {
-                    if (num == Math.Floor(num))
+                    int intNum = (int)num;
+                    if (intNum % 3 == 0 && intNum > maxNumber)
                     {
-                        int intNum = (int)num;
-                        if (intNum % 3 == 0 && intNum > maxNumber)
-                        {
-                            maxNumber = intNum;
-                        }
+                        maxNumber = intNum;
                     }
                 }
-
-                long factorial = 1;
-                for (int i = 2; i <= maxNumber; i++)
-                {
-                    factorial *= i;
-                }
-
-                return factorial;
             }
-              
+
+            long factorial = 1;
+            for (int i = 2; i <= maxNumber; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
         }
+              
+        
     }
 }
